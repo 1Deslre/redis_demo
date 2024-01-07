8e532a85fff317fa303f69cbe5220fafa26bd46d
Deslre <2135232170@qq.com>
2024-01-07 20:04:39 +0800

    登录完成

--- hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserInfoServiceImpl.java
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserInfoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.mapper.UserInfoMapper;
 import com.hmdp.service.IUserInfoService;
@@ -10,6 +14,8 @@ import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -22,20 +28,4 @@ import javax.servlet.http.HttpSession;
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements IUserInfoService {
 
-    @Override
-    public Result sendCode(String phone, HttpSession session) {
-        // 1.校验手机号
-        if (RegexUtils.isPhoneInvalid(phone)) {
-            // 2.如果不符合，返回错误信息
-            return Result.fail("手机号格式错误！");
-        }
-        // 3.符合，生成验证码
-        String code = RandomUtil.randomNumbers(6);
-        // 4.保存验证码到session
-        session.setAttribute("code", code);
-        // 5.发送验证码
-        log.debug("发送短信验证码成功，验证码为：" + code);
-        // 返回ok
-        return Result.ok();
-    }
 }
